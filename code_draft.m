4d3c40ea4053b303a1a5b31248722939e41bde13
dohuan <dohuan@msu.edu>
2016-10-07 09:06:44 -0400

    100716

--- code_draft.m
+++ code_draft.m
@@ -2,3 +2,8 @@ option = struct('begslice',2,'endslice',4,'L',8,'x',7,'N',20,'pts',36,'spacing',
 directory.img = 'C:\Users\dohuan.ME197\Desktop\AortaKit\Image Samples\P11 S04 AAA';
 directory.mask = 'C:\Users\dohuan.ME197\Desktop\AortaKit\Image Samples\P11 S04 AAA Rough Lumen Mask';
 data = featureExtract(directory,option);
+
+
+
+% --- load data to a single training data set
+fieldList{1}='phantomextract1';
