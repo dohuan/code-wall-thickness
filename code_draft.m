c1e822e39a9508696c2b89dee981c63cc3a6fdc9
dohuan <dohuan@msu.edu>
2017-01-17 15:38:49 -0500

    011717

--- code_draft.m
+++ code_draft.m
@@ -1,6 +1,6 @@
 option = struct('begslice',2,'endslice',4,'L',8,'x',7,'N',20,'pts',36,'spacing',1);
-directory.img = 'C:\Users\dohuan.ME197\Desktop\AortaKit\Image Samples\P11 S04 AAA';
-directory.mask = 'C:\Users\dohuan.ME197\Desktop\AortaKit\Image Samples\P11 S04 AAA Rough Lumen Mask';
+directory.img = 'E:\AortaKit\Image Samples\P11 S04 AAA';
+directory.mask = 'E:\AortaKit\Image Samples\P11 S04 AAA Rough Lumen Mask';
 data = featureExtract(directory,option);
 
 
